a813f72b620a77952bb9433caa2b8edd2206ff3d
kalleankah <k.eknefelt@gmail.com>
2018-02-02 12:19:40 +0100

    Lagt till broms

--- Pre-study/Bil.m
+++ Pre-study/Bil.m
@@ -23,6 +23,7 @@ clear drag_area
 
 % Transmission
 gears = [2.66 1.78 1.3 1 0.74 0.5];
+reverse_gear = -2.9;
 differentialRatio = 3.42;
 % Include the differential ratio into the gears
 gears = gears*differentialRatio;
@@ -42,6 +43,7 @@ Force_traction = zeros(1,samples+1);
 current_gear = ones(1,samples); % Gears are changed automatically at redline (for now)
 gearRatio = gears(1);
 deactivate_throttle = false;
+braking_level = 0;
 
 % MEMORY PREALLOCATION
 acceleration = zeros(1, samples);
@@ -61,8 +63,15 @@ gearing_bool = zeros(1, samples);
 i = 1;
 for t = 0:Ts:duration
 % Throttle is turned off if
-% 1. 
-if (deactivate_throttle || gearing_bool(i)==1)
+% 1. RPM is 6000 and gear is max
+% 2. For 200 ms after switching gear
+% 3. When breaking is applied
+
+if(i>5000)
+    braking_level = min(1, 0.001*i);
+end
+
+if (deactivate_throttle || gearing_bool(i)==1 || braking_level>0)
     throttle = 0.0;
 else
     throttle = 1.0; % User input
@@ -84,8 +93,15 @@ total_torque(i) = drive_torque(i) - traction_torque(i);
 % NEW ROLLING RESISTANCE, 0.414938*(0.01+0.00019*velocity(i)) is the
 % coefficient with tyre pressure 2.41 bar.
 rolling_torque(i) = (0.414938*(0.01+0.00019*velocity(i)))*mass*9.81*wheel_radius;
+% Bromskraft
 total_torque(i) = total_torque(i)-rolling_torque(i);
-
+if(i>1 && wheel_velocity(i-1)>1)
+    total_torque(i) = total_torque(i)-3000*braking_level;
+elseif(i>1 && wheel_velocity(i-1)<1)
+    total_torque(i) = total_torque(i)+3000*braking_level;
+else
+    total_torque(i) = total_torque(i);
+end
 % ---- WHEEL VEL/ACC ---- %
 angular_acceleration(i) = total_torque(i)/inertia;
 angular_velocity(i+1) = angular_velocity(i) + angular_acceleration(i)*Ts;
@@ -93,7 +109,6 @@ wheel_velocity(i) = angular_velocity(i+1)*wheel_radius;
 
 % ---- SLIP ---- %
 slip_ratio(i) = (wheel_velocity(i)-velocity(i))/abs(velocity(i));
-
 %If slip ratio is positive (forward)
 if(slip_ratio(i)>=0 && slip_ratio(i)<=0.06)
      force_multiplier = 1-16.67*abs((0.06-slip_ratio(i)));
@@ -111,15 +126,11 @@ end
 % Propelling forces
 Force_traction(i+1) = force_multiplier*load_rear_wheels;
 % Resistive forces
-% Force_rolling = -cRR*velocity(i);
 Force_drag = Cdrag*velocity(i).^2;
-
 % Slutgiltig kraft på bil
 Force_net(i) = Force_traction(i+1) - Force_drag;
-
 % Calculate acceleration
 acceleration(i) = Force_net(i)/(mass*wheel_radius);
-
 % Calculate velocity
 velocity(i+1) = velocity(i) + acceleration(i)*Ts;
 
